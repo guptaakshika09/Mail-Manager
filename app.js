e5e179ea33fc41f1c15fdee193bb7a7c27f4682b
Anshul Goyal <anshulpeehu3@gmail.com>
2021-06-27 21:55:35 +0530

    final

--- app.js
+++ app.js
@@ -358,4 +358,9 @@ app.get('/home',ensureAuthenticated ,function (req,res) {
 //   });
 
 
-app.listen(3000);
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port);
